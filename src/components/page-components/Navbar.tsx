319ae0c7a5cb47ad96ac916477506148af03bcbf
rsarkarr <97837402+rsarkarr@users.noreply.github.com>
2024-11-05 16:16:08 +0530

    add conditional rendering in navbar

--- src/components/page-components/Navbar.tsx
+++ src/components/page-components/Navbar.tsx
@@ -1,27 +1,49 @@
-import { Link } from 'react-router-dom';
-import { Input } from '../ui/input';
-import {ShoppingCart } from 'lucide-react';
+import { Link } from "react-router-dom";
+import { Input } from "../ui/input";
+import { ShoppingCart, User2 } from "lucide-react";
+import Cookies from "js-cookie";
 
 export const Navbar = () => {
+  const token = Cookies.get("sessionToken");
+  console.log(token);
+
   return (
-   <nav className='bg-yellow-300 h-12 shadow-lg'>
-    <div className='flex justify-between items-center px-12'>
+    <nav className="bg-yellow-300 h-12 shadow-lg">
+      <div className="flex justify-between items-center px-12">
         <div>
-            <Link to={"/"} className='font-bold text-2xl'>@discout</Link>
+          <Link to={"/"} className="font-bold text-2xl">
+            @discout
+          </Link>
         </div>
-        <div className='flex mt-1'>
-            <Input placeholder='Search....' className='w-64 bg-white'/>
+        <div className="flex mt-1">
+          <Input placeholder="Search...." className="w-64 bg-white" />
         </div>
         <div>
-            <ul className='flex gap-4'>
-                <Link to={"/user/login"}><li className='bg-white w-14 text-center items-center rounded font-bold hover:bg-black hover:text-yellow-300 transition-all'>Login</li></Link>
-                <Link to={"/cart"}>
-                    <li className='flex bg-white w-16 text-center px-1 rounded hover:bg-black hover:text-yellow-300'><ShoppingCart/>  <span className='ml-1 font-bold'>Cart</span></li>
+          <ul className="flex gap-4">
+            <div>
+              {token ? (
+                <Link to={"/user/profile"}>
+                  <li className="bg-white px-2 text-center items-center rounded font-bold hover:bg-black hover:text-yellow-300 transition-all">
+                    <User2 />
+                  </li>
                 </Link>
-            </ul>
+              ) : (
+                <Link to={"/user/login"}>
+                  <li className="bg-white w-14 text-center items-center rounded font-bold hover:bg-black hover:text-yellow-300 transition-all">
+                    Login
+                  </li>
+                </Link>
+              )}
+            </div>
+
+            <Link to={"/cart"}>
+              <li className="flex bg-white w-16 text-center px-1 rounded hover:bg-black hover:text-yellow-300">
+                <ShoppingCart /> <span className="ml-1 font-bold">Cart</span>
+              </li>
+            </Link>
+          </ul>
         </div>
-    </div>
-   </nav>
+      </div>
+    </nav>
   );
 };
-
