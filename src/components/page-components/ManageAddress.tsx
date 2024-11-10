41bde05e91b45583c650a8e054e91af094021b63
rsarkarr <97837402+rsarkarr@users.noreply.github.com>
2024-11-10 12:37:43 +0530

    show current available address of user

--- src/components/page-components/ManageAddress.tsx
+++ src/components/page-components/ManageAddress.tsx
@@ -1,14 +1,70 @@
-import { AddressAddForm } from "./AddressAddForm"
+import { useEffect, useState } from "react";
+import { AddressAddForm } from "./AddressAddForm";
+import Cookies from "js-cookie";
+import { BACKEND_URI } from "../../../utils/index";
+
+interface userAddress {
+  streetOrLocality: "";
+  city: "";
+  pincode: "";
+  state: "";
+  country: "";
+  mobileNumber: "";
+}
 
 export const ManageAddressComp = () => {
-    return (
+  const token = Cookies.get("sessionToken");
+  const [userAddress, setUserAddress] = useState<userAddress>({
+    streetOrLocality: "",
+    city: "",
+    pincode: "",
+    state: "",
+    country: "",
+    mobileNumber: "",
+  });
+  console.log(userAddress);
+
+  useEffect(() => {
+    const getUserDetails = async () => {
+      try {
+        const res = await fetch(`${BACKEND_URI}/user/find-user-by-jwt`, {
+          method: "POST",
+          headers: {
+            Authorization: `${token}`,
+          },
+        });
+        const response = await res.json();
+        const addressFromResponse = response.user.address;
+        setUserAddress({
+          streetOrLocality: addressFromResponse.streetOrLocality,
+          city: addressFromResponse.city,
+          pincode: addressFromResponse.pincode,
+          state: addressFromResponse.state,
+          country: addressFromResponse.country,
+          mobileNumber: addressFromResponse.mobileNumber,
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getUserDetails();
+  }, []);
+
+  return (
+    <div className="flex flex-col justify-center items-center py-24 space-y-24">
+      <div className="font-semibold">
+        <p>Available address:</p>
         <div>
-            <div>
-                Available address
-            </div>
-            <div>
-                <AddressAddForm />
-            </div>
+            <p>{`${userAddress.streetOrLocality}, ${userAddress.city}`}</p>
+            <p>{`${userAddress.pincode}, ${userAddress.state}`}</p>
+            <p>{`${userAddress.country}`}</p>
+            <p>{`Mobile:- ${userAddress.mobileNumber}`}</p>
         </div>
-    )
-}
+      </div>
+      <div>
+        <AddressAddForm />
+      </div>
+    </div>
+  );
+};
