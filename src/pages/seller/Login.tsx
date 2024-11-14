dce42b7170e01b42d548d18c2f8100173affc482
rsarkarr <97837402+rsarkarr@users.noreply.github.com>
2024-11-15 02:58:25 +0530

    redirect users to dashboard if they have session jwt

--- src/pages/seller/Login.tsx
+++ src/pages/seller/Login.tsx
@@ -1,6 +1,19 @@
 import { LoginForm } from "@/components/page-components/seller/LoginForm"
+import Cookies from "js-cookie";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+
+  const navigate = useNavigate();
+  const token = Cookies.get("sessionToken");
+
+  useEffect(() => {
+    if (token) {
+      navigate("/seller/dashboard");
+    }
+  }, []);
+
   return (
     <>
     <LoginForm />
--- src/pages/seller/Register.tsx
+++ src/pages/seller/Register.tsx
@@ -1,11 +1,23 @@
-import { RegisterForm } from "@/components/page-components/seller/RegisterForm"
+import { RegisterForm } from "@/components/page-components/seller/RegisterForm";
+import Cookies from "js-cookie";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Register = () => {
+  const navigate = useNavigate();
+  const token = Cookies.get("sessionToken");
+
+  useEffect(() => {
+    if (token) {
+      navigate("/seller/dashboard");
+    }
+  }, []);
+
   return (
     <>
-        <RegisterForm />
+      <RegisterForm />
     </>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
