c7b5ab969adcd42c47f895428ff789a1675ce1ce
rsarkarr <97837402+rsarkarr@users.noreply.github.com>
2024-11-16 15:00:57 +0530

    add primary secondary and tertiary image upload func

--- src/components/page-components/seller/AddProducts.tsx
+++ src/components/page-components/seller/AddProducts.tsx
@@ -10,17 +10,36 @@ import {
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { BACKEND_URI } from "../../../../utils/index";
+import { useState } from "react";
+
+interface imageFields {
+  primary: "",
+  secondary: "",
+  tertiary: ""
+}
 
 export const AddProducts = () => {
+  // create few states for form fields
+  // like: for specs
+  // react hook form maybe not suitable for this one.
 
+  const [specs, setSpecs] = useState([""]);
 
-  // upload image
-  const uploadImage = async (e: React.ChangeEvent<HTMLInputElement>) => {
+  const [primaryImg, setPrimaryImg] = useState("")
+  const [secondary, setSecondaryImg] = useState("")
+  const [tertiary, setTertiaryImg] = useState("")
+
+  console.log(primaryImg);
+  console.log(secondary);
+  console.log(tertiary);
+  
+  // upload primary image
+  const uploadPImage = async (e: React.ChangeEvent<HTMLInputElement>) => {
     // onChange target the file
     const files = e.target.files;
 
-    console.log(typeof(files));
-    
+    console.log(typeof files);
+
     // if no file available error
     if (!files) {
       console.error("No file selected");
@@ -28,8 +47,8 @@ export const AddProducts = () => {
     }
     // create formData
     const formData = new FormData();
-    
-    // append files array 
+
+    // append files array
     formData.append("file", files[0]);
     // send http req
     try {
@@ -38,15 +57,89 @@ export const AddProducts = () => {
         body: formData, // send here the form data not the files
       });
 
-      const response = await res.json()
-  
+      const response = await res.json();
+
       if (response.success !== true) {
         throw new Error(`Error: ${res.status} - ${res.statusText}`);
       }
-      
+
       console.log("Upload successful:", response);
+      setPrimaryImg(response.url)
       console.log(formData);
-      
+    } catch (error) {
+      console.error("File upload failed:", error);
+    }
+  };
+  // upload secondary image
+  const uploadSImage = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    // onChange target the file
+    const files = e.target.files;
+
+    console.log(typeof files);
+
+    // if no file available error
+    if (!files) {
+      console.error("No file selected");
+      return;
+    }
+    // create formData
+    const formData = new FormData();
+
+    // append files array
+    formData.append("file", files[0]);
+    // send http req
+    try {
+      const res = await fetch(`${BACKEND_URI}/seller/upload`, {
+        method: "POST",
+        body: formData, // send here the form data not the files
+      });
+
+      const response = await res.json();
+
+      if (response.success !== true) {
+        throw new Error(`Error: ${res.status} - ${res.statusText}`);
+      }
+
+      console.log("Upload successful:", response);
+      setSecondaryImg(response.url)
+      console.log(formData);
+    } catch (error) {
+      console.error("File upload failed:", error);
+    }
+  };
+  // upload tertiary image
+  const uploadTImage = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    // onChange target the file
+    const files = e.target.files;
+
+    console.log(typeof files);
+
+    // if no file available error
+    if (!files) {
+      console.error("No file selected");
+      return;
+    }
+    // create formData
+    const formData = new FormData();
+
+    // append files array
+    formData.append("file", files[0]);
+    // send http req
+    try {
+      const res = await fetch(`${BACKEND_URI}/seller/upload`, {
+        method: "POST",
+        body: formData, // send here the form data not the files
+      });
+
+      const response = await res.json();
+
+      if (response.success !== true) {
+        throw new Error(`Error: ${res.status} - ${res.statusText}`);
+      }
+
+      console.log("Upload successful:", response);
+      setTertiaryImg(response.url)
+      console.log(formData);
     } catch (error) {
       console.error("File upload failed:", error);
     }
@@ -69,11 +162,11 @@ export const AddProducts = () => {
             <Input placeholder="Price" />
             <div>
               <label>Primary</label>
-              <Input type="file" onChange={uploadImage}/>
+              <Input type="file" onChange={uploadPImage} />
               <label>Secondary</label>
-              <Input type="file" onChange={uploadImage}/>
+              <Input type="file" onChange={uploadSImage} />
               <label>Third</label>
-              <Input type="file" />
+              <Input type="file" onChange={uploadTImage}/>
             </div>
             <AlertDialogDescription></AlertDialogDescription>
           </AlertDialogHeader>
